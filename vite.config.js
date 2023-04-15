5e474f4cb4f1f8ce49d28dbb346df40171625535
anif 21ftt1288 <21ftt1288@student.pb.edu.bn>
2023-04-15 08:47:28 +0800

    recreated vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite"
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+    base: '/21FTT1288-as3/',
+    plugins: [vue()]
+})
